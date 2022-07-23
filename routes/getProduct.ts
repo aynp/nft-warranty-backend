623d2599073b0d170d053415e5f59c5a6965a6ea
Aryan <aryan.pathania2003@gmail.com>
2022-07-23 11:48:46 +0530

    Added getProduct route

--- routes/getProduct.ts
+++ routes/getProduct.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
+import Product from '../models/Product';
 
 const getProduct = async (req: Request, res: Response) => {
   try {
-    const { productID } = req.body;
+    const { productID } = req.params;
+    console.log('Get request for - ', productID);
+
+    const product = await Product.findOne({ _id: productID });
+
+    res.status(200).json({
+      success: true,
+      product,
+      message: `product${product ? '' : ' not'} found`,
+    });
   } catch ({ message }: any) {
     res.status(400).json({
       success: false,
--- routes/routes.ts
+++ routes/routes.ts
@@ -4,7 +4,7 @@ import getProduct from './getProduct';
 
 const router = express.Router();
 
-router.route('/getProduct').get(getProduct);
+router.route('/getProduct/:productID').get(getProduct);
 router.route('/addProduct').post(addProduct);
 
 export default router;
